5ec35e52bc8a588fb1facff8f8187a86e786e56c
redwan09uk <redwan.ahmed09@gmail.com>
2024-09-19 00:48:40 +0100

    added shorcut

--- js/login.js
+++ js/login.js
@@ -6,14 +6,48 @@
 
 //event: event parameter use when something used in browser in event that auotmatically select the event parameter, someone write it as a 'e' sometime.
 
-
+/*
 //sep1: set event handler
 document.getElementById('buttton-login').addEventListener('click',function(event){
     //step 2: prevent dfault behavior-(prevent reloading browser) which used to prevent browser from reload automatically
     event.preventDefault(); 
-    console.log('login button clicked');
     
-    //step3: get the phone number
-    const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    //step3: get the phone number and pin number
+    const phoneNumber = document.getElementById('phone-number').value; //value added here to get the a result after clicking
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber)
+
+    //step 4: validate the phone number and pin
+    //this is temporary , we should not do like this
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('you are logged in')
+        //step 5: allow user to use the website
+    } else {
+        alert('wrong phone number or pin');
+    }
 })
+    */
+   //short cut without much explanation
+
+   document.getElementById('buttton-login').addEventListener('click',function(event){
+    event.preventDefault();
+     
+     //get the phone number and pin number
+
+   const phoneNumber = document.getElementById('phone-number').value;
+   const pinNumber = document.getElementById('pin-number').value
+
+   console.log(phoneNumber, pinNumber);
+
+   //bad way to validate, it is only for use temporary
+
+   if(phoneNumber === '5' && pinNumber=== '1234'){
+    console.log('you are logged in');
+    window.location.href= '/h';//it is not working need to fix
+   } else {
+    alert('wrong pin or phone number')
+   }
+
+   });
+
+  
